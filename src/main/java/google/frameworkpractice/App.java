e5c2a58d3813013dcf3057149b5fb6b0710a4038
SREENU <SREENU@SREENU-PC>
2019-12-18 12:33:23 +0530

    hello

---
+++ src/main/java/google/frameworkpractice/App.java
@@ -0,0 +1,13 @@
+package google.frameworkpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/google/frameworkpractice/Searchword.java
@@ -0,0 +1,16 @@
+package google.frameworkpractice;
+
+import org.testng.annotations.Test;
+
+public class Searchword  extends Launchapplication{
+	@Test
+	public static void method1() {
+		Commonmethods.testmethod1();
+	
+	}
+	@Test
+	public static void method2() {
+		Commonmethods.testmethod2();
+	}
+
+}
